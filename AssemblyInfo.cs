00be43f8e2f33403221dfeb379b51a71ed603aae
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-03-03 22:23:19 +0100

    Shared assemblyinfo

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImageProcessingCore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Glass.Basics")]
+[assembly: AssemblyDescription("Glass Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImageProcessingCore")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("JMN")]
+[assembly: AssemblyProduct("Glass.Basics")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,8 +20,18 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("056e7401-e413-4e50-9009-7fe1c69dee4f")]
+[assembly: Guid("939be462-a9d4-4412-888c-8fbbf3c0a48a")]
 
 // Version information for an assembly consists of the following four values:
 //
