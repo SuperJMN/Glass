05bc98cc54764c3ee4b90f59fbee28dfadba637d
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-03-03 22:35:35 +0100

    Cosa tuneada

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -19,16 +17,6 @@ using System.Windows;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page, 
-    // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page, 
-    // app, or any theme specific resource dictionaries)
-)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("939be462-a9d4-4412-888c-8fbbf3c0a48a")]
