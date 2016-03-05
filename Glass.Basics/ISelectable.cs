e3a2c9c12bdf157af2289f8f29c9dba5d6d9743a
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-03-05 19:00:36 +0100

    Created packages and updated solution and projects

--- Glass.Basics/ISelectable.cs
+++ Glass.Basics/ISelectable.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using Glass.Basics.Core;
+﻿namespace Glass.Basics {
 
-namespace Glass.Basics {
+    using System;
+
     public interface ISelectable {
         bool IsSelected { get; set; }
-        event EventHandler<EventArgs<bool>> IsSelectedChanged;
+        event EventHandler<bool> IsSelectedChanged;
     }
 }
