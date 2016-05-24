202b8be8ab7dad7083acad3c810d9ecfa2e201f5
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-05-24 12:19:03 +0200

    Improved MessagingToolkit

--- Glass.Barcodes.MessagingToolkit/MessagingToolkitZoneBasedBarcodeReader.cs
+++ Glass.Barcodes.MessagingToolkit/MessagingToolkitZoneBasedBarcodeReader.cs
@@ -1,7 +1,11 @@
 ﻿namespace Glass.Barcodes.MessagingToolkit
 {
+    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Reactive.Linq;
+    using System.Reactive.Threading.Tasks;
+    using System.Threading.Tasks;
     using System.Windows.Media.Imaging;
     using global::MessagingToolkit.Barcode;
     using Imaging;
@@ -15,20 +19,22 @@
         public IEnumerable<RecognitionResult> Recognize(BitmapSource bitmap, ZoneConfiguration config)
         {
             var writeableBitmap = new WriteableBitmap(bitmap.Clone());
-            string text;
-            try
-            {
-                var result = barcodeReader.Decode(writeableBitmap, new Dictionary<DecodeOptions, object>());
-                text = result.Text;
-            }
-            catch
-            {
-                text = null;
-            }
+            writeableBitmap.Freeze();
+
+            var recognizer = Task.Run(() => barcodeReader.Decode(writeableBitmap, new Dictionary<DecodeOptions, object>()))
+                .ToObservable();
+
+            var result = recognizer
+                .Timeout(TimeSpan.FromSeconds(2))
+                .Catch<Result, TimeoutException>(arg => Observable.Return<Result>(null))
+                .Catch<Result, NotFoundException>(arg => Observable.Return<Result>(null));
+
+            var text = result.ToTask().Result?.Text;
 
             yield return new RecognitionResult(text, 1D);
         }
 
-        public IEnumerable<ImageTarget> ImageTargets => new Collection<ImageTarget> { new ImageTarget { Symbology = Symbology.Barcode, FilterTypes = FilterType.All } };
+        public IEnumerable<ImageTarget> ImageTargets
+            => new Collection<ImageTarget> {new ImageTarget {Symbology = Symbology.Barcode, FilterTypes = FilterType.All}};
     }
 }
