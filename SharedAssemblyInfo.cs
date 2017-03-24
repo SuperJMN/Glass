57e87bad21e6b7fa2daa222503b2a5f4078595f7
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2017-03-24 11:05:43 +0100

    Version para ClearImage

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("JMN")]
 [assembly: AssemblyProduct("Glass")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
